ebe3e7ae124cf2811cd40b047732320d2caafa3c
thoraninchaiya <thoraninchaiya@gmail.com>
2021-07-23 10:29:10 +0700

    edit card & product

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-carousel cycle height="650" hide-delimiter-background show-arrows-on-hover>
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+      <v-carousel-item v-for="(slide, i) in colors" :key="i">
         <v-sheet :color="colors[i]" height="100%">
           <v-row class="fill-height" align="center" justify="center">
             <!-- <div class="text-h2">
@@ -22,23 +22,23 @@
               </div>
             </v-card-title>
             <v-card-text class="d-flex align-content-start flex-wrap">
-              <Card v-for="post in carddata" :key="post.id" :post="post" />
+              <MainCard v-for="post in carddata" :key="post.id" :post="post" />
             </v-card-text>
           </v-card>
         </v-app>
       </div>
-  </div>
+    </div>
 
   </v-app>
 </template>
 
 <script>
 import axios from 'axios'
-import Card from '@/components/Card'
+import MainCard from '@/components/MainCard'
 
   export default {
     components:{
-      Card
+      MainCard
     },
     data () {
       return {
@@ -48,19 +48,10 @@ import Card from '@/components/Card'
           'pink darken-2',
           'red lighten-1',
           'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
+          'green',
         ],
         carddata: [],
       }
-    },
-    head: {
-        title: 'รายการโพสต์'
     },
     async created() {
       this.carddata = await axios.get('https://jsonplaceholder.typicode.com/posts')
--- pages/product.vue
+++ pages/product.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-      product
-  </div>
+  <v-app>
+    <div>
+      <v-card-title>
+        title 1
+      </v-card-title>
+    </div>
+
+    <div>
+      <v-card>
+        <v-card-title>
+          title 2
+        </v-card-title>
+      </v-card>
+    </div>
+  </v-app>
 </template>
 
 <script>
