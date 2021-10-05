e25c56b2aa94fee6f94a89f273e3c454bb1d8153
thoraninchaiya <thoraninchaiya@gmail.com>
2021-10-05 17:26:35 +0700

    [n] apexcharts, [u] admin

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="d-flex">
+    <div class="ma-12 pa-12">
+        <AdminCom-Core-Nav/>
+    </div>
+    <v-main>
+        <Nuxt />
+    </v-main>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/admin/product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      product
+  </div>
+</template>
+
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
+
+<style>
+
+</style>
