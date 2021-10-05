a2e53a1949fe3027a682966e705fee8f9dcbf48e
thoraninchaiya <thoraninchaiya@gmail.com>
2021-10-05 18:47:21 +0700

    [u] v-app

--- components/AdminCom/Core/Nav.vue
+++ components/AdminCom/Core/Nav.vue
@@ -1,45 +1,47 @@
 <template>
-<v-card>
-    <v-navigation-drawer permanent expand-on-hover>
-        <v-list>
-            <v-list-item class="px-2">
-                <v-list-item-avatar>
-                    <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-                </v-list-item-avatar>
-            </v-list-item>
-            <v-list-item link>
-                <v-list-item-content>
-                    <v-list-item-title class="text-h6">
-                        Sandra Adams
-                    </v-list-item-title>
-                    <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-
-        <v-list nav dense>
-            <v-list-item v-for="(item, i) in adminlink" :key="i" :to="item.to">
-                <v-list-item-icon>
-                    <v-icon>{{item.icon}}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>{{item.title}}</v-list-item-title>
-            </v-list-item>
-        </v-list>
-
-        <v-divider></v-divider>
-        
-        <v-list nav dense>
-            <v-list-item to="/">
-                <v-list-item-icon>
-                    <v-icon>mdi-account-arrow-right</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-            </v-list-item>
-        </v-list>
-    </v-navigation-drawer>
-</v-card>
+<v-app>
+    <v-card>
+        <v-navigation-drawer permanent expand-on-hover>
+            <v-list>
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                        <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                    </v-list-item-avatar>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-content>
+                        <v-list-item-title class="text-h6">
+                            Sandra Adams
+                        </v-list-item-title>
+                        <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+    
+            <v-divider></v-divider>
+    
+            <v-list nav dense>
+                <v-list-item v-for="(item, i) in adminlink" :key="i" :to="item.to">
+                    <v-list-item-icon>
+                        <v-icon>{{item.icon}}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>{{item.title}}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+    
+            <v-divider></v-divider>
+    
+            <v-list nav dense>
+                <v-list-item to="/">
+                    <v-list-item-icon>
+                        <v-icon>mdi-account-arrow-right</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </v-card>
+</v-app>
 </template>
 
 <script>
